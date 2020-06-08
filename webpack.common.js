6de32c6a9c9a3584b6256e7f82d9e653c33017c4
Antonio Perez <00antonio.perez00@gmail.com>
2020-06-08 12:45:35 -0500

    Split webpack env

---
+++ webpack.common.js
@@ -0,0 +1,63 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack')
+const path = require('path');
+
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: '!!compile-ejs-loader!./src/templates/index.ejs',
+    title: 'DronefaceRouter',
+    favicon: './src/images/favicon.png'
+  }),
+  new webpack.DefinePlugin({
+    'process.env': {
+      FIREBASE_API_KEY: JSON.stringify(process.env.FIREBASE_API_KEY),
+      FIREBASE_AUTH_DOMAIN: JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+      FIREBASE_DATABASE_URL: JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+      FIREBASE_PROJECT_ID: JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+      FIREBASE_STORAGE_BUCKET: JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+      FIREBASE_MESSAGING_SENDER_ID: JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+      FIREBASE_APP_ID: JSON.stringify(process.env.FIREBASE_APP_ID),
+      FIREBASE_MEASUREMENT_ID: JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID),
+    }
+  })
+];
+
+const rules = [
+	{
+		test: /\.(ts|js)x?$/,
+		exclude: /node_modules/,
+		use: {
+			loader: 'babel-loader'
+		},
+	},
+	{
+		test: /\.css$/,
+		use: ['style-loader', 'css-loader']
+	},
+	{
+		test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+		exclude: /node_modules/,
+		use: ['file-loader?name=[name].[ext]']
+	},
+	{
+		test: /\.ejs$/,
+		use: ['compile-ejs-loader']
+	}
+];
+
+
+module.exports = {
+	entry: './src/index',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.[hash].js',
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: rules
+	},
+	plugins: plugins
+}
---
+++ webpack.dev.js
@@ -0,0 +1,17 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devServer: {
+    historyApiFallback: true,
+    host: '0.0.0.0',
+    port: 1023,
+    inline: true,
+    hot: true
+  },
+  watch: true,
+  watchOptions: {
+      poll: true
+  }
+});
